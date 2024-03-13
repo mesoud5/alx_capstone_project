17375f904318340df22a3c2a149eeb0ae1aef384
John Doe <mesoudtemam5@gmail.com>
2024-03-12 23:28:10 -0700

    commit

---
+++ capstone_project/statics/js/home.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const backgrounds = [
+        "url('../statics/images/Image 1.png')",
+        "url('../statics/images/projects.webp')",
+        "url('../statics/images/skills.jpeg')"
+        // Add more image paths as needed
+    ];
+
+    let currentIndex = 0;
+    const body = document.body;
+
+    function changeBackground() {
+        body.style.opacity = 0;
+        setTimeout(function() {
+            currentIndex = (currentIndex + 1) % backgrounds.length;
+            body.style.backgroundImage = backgrounds[currentIndex];
+            body.style.opacity = 1;
+        }, 1000); // Wait for transition to complete (1000ms = 1s)
+    }
+
+    // Change background every 7 seconds
+    setInterval(changeBackground, 7000);
+});

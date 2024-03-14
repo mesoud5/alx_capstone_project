71c00f433115cd7f63b95e4c57bc292ea08c4c18
John Doe <mesoudtemam5@gmail.com>
2024-03-14 04:18:26 -0700

    project responsiveness

---
+++ capstone_project/static/js/projects.js
@@ -0,0 +1,10 @@
+
+// projects.js
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const upperNav = document.getElementById("upper-nav");
+
+    hamburgerMenu.addEventListener('click', function () {
+        upperNav.classList.toggle('show');
+    });
+});

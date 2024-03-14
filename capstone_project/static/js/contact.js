21a19b427c6540822e7c215f19ac1f3961465927
John Doe <mesoudtemam5@gmail.com>
2024-03-14 04:41:55 -0700

    commit

---
+++ capstone_project/static/js/contact.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const upperNav = document.getElementById("upper-nav");
+
+    hamburgerMenu.addEventListener('click', function () {
+        upperNav.classList.toggle('show');
+    });
+});
